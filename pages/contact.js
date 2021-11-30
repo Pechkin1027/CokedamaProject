33e6ea7397ec6b0d8cd4d275396c27445e7d5d48
Msabdulla17 <33076735+Msabdulla17@users.noreply.github.com>
2021-11-30 07:35:36 -0500

    more fixes

--- pages/contact.js
+++ pages/contact.js
@@ -9,26 +9,54 @@ import { AllProducts } from "../data/Category";
 export default function contact() {
   return (
     <PageLayout type={"CONTACT"}>
-      <div className="contactBanner">
+      <div className="aboutBanner">
         <div className="bannerHeader">
           <h2>Contact Us</h2>
         </div>
         <div className="bannerContent">
           <img src="https://i.ibb.co/1bQSDG2/kokedama-header-1024x683.jpg" width="50%" />
           <div className="bannerText">
-              <a
-                href="https://www.facebook.com/cokedamainteriorplantscaping"
-                class="fbLink"
-              >
-                <div className="facebook">
-                  <img src="Facebook.png" width="25" height="25" />
-                  <h3>https://www.facebook.com/cokedamainteriorplantscaping</h3>
-                </div>
-              </a>
-              <h3>+94 71 412 0033</h3>
+            <p>
+            Phone: +94 71 412 0033
+            </p>
+            <p>
+            Email: cokedamainterior@gmail.com
+            </p>
           </div>
         </div>
       </div>
+      <div className="aboutStory">
+        <div className="storyHeader">
+          <h2>Send us a message</h2>
+        </div>
+        <div class="body">
+            <div id="contact-form">
+            <form id="contact" class="form-class" method="post" action="contact-form-process.php"> 
+                <div class="form-group">
+                    <label for="Name" class="label">Your name</label>
+                    <div class="input-group">
+                        <input type="text" id="Name" name="Name" class="form-control" required></input>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="Email" class="label">Your email address</label>
+                    <div class="input-group">
+                        <input type="email" id="Email" name="Email" class="form-control" required></input>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="Message" class="label">Your message</label>
+                    <div class="input-group">
+                        <textarea id="Message" name="Message" class="form-control" rows="6" maxlength="3000" required></textarea>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <button type="submit" id="button" class="button">Send Message</button>
+                </div>
+            </form>
+            </div>
+        </div>
+        </div>
     </PageLayout>
   );
 }
