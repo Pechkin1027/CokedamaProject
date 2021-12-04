3ec5e952ab2945f4ef69aeb2f66e31ff8c379b4f
ABA03 <mjb88@njit.edu>
2021-12-03 20:48:25 -0500

    Matt's upload

--- pages/contact.js
+++ pages/contact.js
@@ -5,58 +5,79 @@ import styled from "styled-components";
 import ProductGrid from "../components/Landing/ProductGrid";
 import CategoryGrid from "../components/Landing/CategoryGrid";
 import { AllProducts } from "../data/Category";
+import { BiRepeat } from "react-icons/bi";
 
+//Javascript
 export default function contact() {
   return (
     <PageLayout type={"CONTACT"}>
       <div className="aboutBanner">
         <div className="bannerHeader">
           <h2>Contact Us</h2>
-        </div>
-        <div className="bannerContent">
-          <img src="https://i.ibb.co/1bQSDG2/kokedama-header-1024x683.jpg" width="50%" />
-          <div className="bannerText">
-            <p>
-            Phone: +94 71 412 0033
-            </p>
-            <p>
-            Email: cokedamainterior@gmail.com
-            </p>
+          <div className="contactBanner">
+            <img
+              src="https://i.ibb.co/1bQSDG2/kokedama-header-1024x683.jpg"
+              width="100%"
+            />
+          </div>
+          {/* Form group 1 */}
+          <div class="col-md-6 col-sm-12">
+            <div class="contact-block">
+              <div class="form-group-1">
+                <div class="input-group">
+                  <label for="Name" class="label">
+                    Your name
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    class="control-form-control"
+                    required
+                  ></input>
+                </div>
+                <div class="input group">
+                  <label for="Email" class="label">
+                    Your Email Address
+                  </label>
+                  <input
+                    type="text"
+                    id="email"
+                    class="control-form-control"
+                    required
+                  ></input>
+                </div>
+                <div class="input group">
+                  <label for="Subject" class="label">
+                    Subject
+                  </label>
+                  <input
+                    type="text"
+                    id="email"
+                    class="control-form-control"
+                  ></input>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-12 col-sm-12">
+            <div class="form-group-2">
+              <label for="Message" class="label">
+                Message
+              </label>
+              <div class="input-group">
+                <textarea
+                  name="user message"
+                  class="for control"
+                  rows="5"
+                  columns="4"
+                  maxlength="3000"
+                  placeholder="Your message"
+                ></textarea>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      <div className="aboutStory">
-        <div className="storyHeader">
-          <h2>Send us a message</h2>
-        </div>
-        <div class="body">
-            <div id="contact-form">
-            <form id="contact" class="form-class" method="post" action="components/contact-form-process.php"> 
-                <div class="form-group">
-                    <label for="Name" class="label">Your name</label>
-                    <div class="input-group">
-                        <input type="text" id="Name" name="Name" class="form-control" required></input>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="Email" class="label">Your email address</label>
-                    <div class="input-group">
-                        <input type="email" id="Email" name="Email" class="form-control" required></input>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="Message" class="label">Your message</label>
-                    <div class="input-group">
-                        <textarea id="Message" name="Message" class="form-control" rows="6" maxlength="3000" required></textarea>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <button type="submit" id="button" class="button">Send Message</button>
-                </div>
-            </form>
-            </div>
-        </div>
-        </div>
     </PageLayout>
   );
 }
