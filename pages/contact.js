866c853c8c0caa08bf0eb2d9b4b8ac3c4759f3c1
BuildTools <unconfigured@null.spigotmc.org>
2021-12-03 21:53:47 -0500

    .

--- pages/contact.js
+++ pages/contact.js
@@ -11,73 +11,41 @@ import { BiRepeat } from "react-icons/bi";
 export default function contact() {
   return (
     <PageLayout type={"CONTACT"}>
-      <div className="aboutBanner">
-        <div className="bannerHeader">
+      <div className="contactBanner">
+        <div className="contactImg">
+          <img src="https://i.ibb.co/1bQSDG2/kokedama-header-1024x683.jpg" width="100%"/>
+        </div>
+        <div className="contactHeader">
           <h2>Contact Us</h2>
-          <div className="contactBanner">
-            <img
-              src="https://i.ibb.co/1bQSDG2/kokedama-header-1024x683.jpg"
-              width="100%"
-            />
-          </div>
-          {/* Form group 1 */}
-          <div class="col-md-6 col-sm-12">
-            <div class="contact-block">
-              <div class="form-group-1">
-                <div class="input-group">
-                  <label for="Name" class="label">
-                    Your name
-                  </label>
-                  <input
-                    type="text"
-                    id="name"
-                    class="control-form-control"
-                    required
-                  ></input>
-                </div>
-                <div class="input group">
-                  <label for="Email" class="label">
-                    Your Email Address
-                  </label>
-                  <input
-                    type="text"
-                    id="email"
-                    class="control-form-control"
-                    required
-                  ></input>
-                </div>
-                <div class="input group">
-                  <label for="Subject" class="label">
-                    Subject
-                  </label>
-                  <input
-                    type="text"
-                    id="email"
-                    class="control-form-control"
-                  ></input>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-12 col-sm-12">
-            <div class="form-group-2">
-              <label for="Message" class="label">
-                Message
-              </label>
+        </div>
+      </div>
+        {/* Form group 1 */}
+        <div class="col-md-6 col-sm-12">
+          <div class="contact-block">
+            <div class="form-group-1">
               <div class="input-group">
-                <textarea
-                  name="user message"
-                  class="for control"
-                  rows="5"
-                  columns="4"
-                  maxlength="3000"
-                  placeholder="Your message"
-                ></textarea>
+                <label for="Name" class="label"> Your name </label>
+                <input type="text" id="name" class="control-form-control" required></input>
+              </div>
+              <div class="inputGroup">
+                <label for="Email" class="label"> Your Email Address </label>
+                <input type="text" id="email" class="control-form-control" required></input>
+              </div>
+              <div class="inputGroup">
+                <label for="Subject" class="label"> Subject </label>
+                <input type="text" id="email" class="control-form-control"></input>
               </div>
             </div>
           </div>
         </div>
-      </div>
+        <div class="col-md-12 col-sm-12">
+          <div class="form-group-2">
+            <label for="Message" class="label"> Message </label>
+            <div class="inpuGroup">
+              <textarea name="user message" class="for control" rows="5" columns="4" maxlength="3000" placeholder="Your message"></textarea>
+            </div>
+          </div>
+        </div>
     </PageLayout>
   );
 }
