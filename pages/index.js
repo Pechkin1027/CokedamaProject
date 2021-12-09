280da4c9cee987a128a2d2a5a4de4b4b39f4f23f
BuildTools <unconfigured@null.spigotmc.org>
2021-12-09 15:00:22 -0500

    homepage changes and contact update

--- pages/index.js
+++ pages/index.js
@@ -62,86 +62,80 @@ export default function home() {
       />
       <div className="vision-section">
         <h2>Our Company's Mission</h2>
-        <div className="vision-row1">
-          <div className="vision-box">
-            <img src={"Awareness.png"} />
-            <h2>Awareness</h2>
-            <p>
-              Create a favorable perception of interior plantscaping and
-              increase awareness of careers working with plants.
-            </p>
+          <div className = "boxes">
+            <div className="vision-box">
+              <img src={"Awareness.png"} />
+              <h2>Awareness</h2>
+              <p>
+                Create a favorable perception of interior plantscaping and
+                increase awareness of careers working with plants.
+              </p>
+            </div>
+            <div className="vision-box">
+              <img src="Education.png" />
+              <h2>Education</h2>
+              <p>
+                Engage and excite students and professionals in interior design
+                fields about horticulture.
+              </p>
+            </div>
+            <div className="vision-box">
+              <img src="Empower.png" />
+              <h2>Empowerment</h2>
+              <p>
+                Empower creative moms by creating and popularizing business
+                opportunities.
+              </p>
+            </div>
+            <div className="vision-box">
+              <img src="Partnership.png" />
+              <h2>Partnerships</h2>
+              <p>
+                Cultivate and strengthen partnerships and collaborations, while
+                diversifying and broadening the base of advocates and supporters.
+              </p>
+            </div>
+            <div className="vision-box">
+              <img src="Eco Friendly.png" />
+              <h2>Eco-Friendly</h2>
+              <p>
+                Popularize eco-friendly concepts in the interior design field by
+                advocating the use of recycled materials.
+              </p>
+            </div>
+            <div className="vision-box">
+              <img src="Resource Dev.png" />
+              <h2>Resource Development</h2>
+              <p>
+                Create educational resources such as plant databases, valuable
+                tips, and guides for the community.
+              </p>
+            </div>
+            <div className="vision-box">
+              <img src="Workforce.png" />
+              <h2>Workforce Developement</h2>
+              <p>
+                Increase the skilled workforce in plantscaping and develop a
+                strong pipeline of future talent.
+              </p>
+            </div>
           </div>
-          <div className="vision-box">
-            <img src="Education.png" />
-            <h2>Education</h2>
-            <p>
-              Engage and excite students and professionals in interior design
-              fields about horticulture.
-            </p>
-          </div>
-          <div className="vision-box">
-            <img src="Empower.png" />
-            <h2>Empowerment</h2>
-            <p>
-              Empower creative moms by creating and popularizing business
-              opportunities.
-            </p>
-          </div>
-          <div className="vision-box">
-            <img src="Partnership.png" />
-            <h2>Partnerships</h2>
-            <p>
-              Cultivate and strengthen partnerships and collaborations, while
-              diversifying and broadening the base of advocates and supporters.
-            </p>
-          </div>
-        </div>
-        <div className="vision-row2">
-          <div className="vision-box">
-            <img src="Eco Friendly.png" />
-            <h2>Eco-Friendly</h2>
-            <p>
-              Popularize eco-friendly concepts in the interior design field by
-              advocating the use of recycled materials.
-            </p>
-          </div>
-          <div className="vision-box">
-            <img src="Resource Dev.png" />
-            <h2>Resource Development</h2>
-            <p>
-              Create educational resources such as plant databases, valuable
-              tips, and guides for the community.
-            </p>
-          </div>
-          <div className="vision-box">
-            <img src="Workforce.png" />
-            <h2>Workforce Developement</h2>
-            <p>
-              Increase the skilled workforce in plantscaping and develop a
-              strong pipeline of future talent.
-            </p>
-          </div>
-        </div>
       </div>
       <MainCont>
         <div className = "bannerOne">
           <div className = "oneText">
-            <h2>Kokedama</h2>
-            <p>A free-form variant of traditional bonsai that is easy to try at home</p>
+            <h2>Our Vision</h2>
+            <p>To build a community that appreciates and values eco-friendly interior designs and to raise awareness of the importance of eco-friendly interior decorations through raising awareness, education, workforce development, partnerships, and resource development.</p>
           </div>
           <form action = "http://localhost:3000/category?type=MoneyPlants">
               <input type = "submit" value = "Shop Now"/>
           </form>
-          <img src = "https://i.ibb.co/vwXndX8/shutterstock-1825228952.jpg" />
         </div>
-        {/* <JumboSaleDisplay
-          img="https://i.ibb.co/vwXndX8/shutterstock-1825228952.jpg"
-          url="/category?type=MoneyPlants"
-          title="Kokedama"
-          text="A free-form variant of traditional bonsai that is easy to try at home"
-        /> */}
         <ProductGrid title={"Categories"} type={"CATEGORY"} data={category1} />
         <div className = "bannerTwo">
+          <form action = "http://localhost:3000/category?type=MoneyPlants">
+              <input type = "submit" value = "Shop Now"/>
+          </form>
           <div className = "twoText">
             <h2>Our Goal</h2>
             <p>To promote eco-friendly interior designing and
@@ -149,20 +143,7 @@ export default function home() {
           meaningful creative work to financially support them and their
           families.</p>
           </div>
-          <form action = "http://localhost:3000/category?type=MoneyPlants">
-              <input type = "submit" value = "Shop Now"/>
-          </form>
-          <img src = "https://i.ibb.co/6N0r0ws/shutterstock-1711748359.jpg" />
         </div>
-        {/* <JumboSaleDisplay
-          img="https://i.ibb.co/6N0r0ws/shutterstock-1711748359.jpg"
-          url="/category?type=SnakePlants"
-          title="Our Goal"
-          text="To promote eco-friendly interior designing and
-          inspire people to pursue careers working with plants while finding
-          meaningful creative work to financially support them and their
-          families."
-        /> */}
       </MainCont>
     </PageLayout>
   );
