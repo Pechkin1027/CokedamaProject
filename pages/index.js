016e6e744285f18656ba2fb4a7ebc2fa4f906da3
unknown <ahmedz123645@gmail.com>
2021-12-13 21:00:32 -0500

    Responsive

--- pages/index.js
+++ pages/index.js
@@ -62,86 +62,94 @@ export default function home() {
       />
       <div className="vision-section">
         <h2>Our Company's Mission</h2>
-          <div className = "boxes">
-            <div className="vision-box">
-              <img src={"Awareness.png"} />
-              <h2>Awareness</h2>
-              <p>
-                Create a favorable perception of interior plantscaping and
-                increase awareness of careers working with plants.
-              </p>
-            </div>
-            <div className="vision-box">
-              <img src="Education.png" />
-              <h2>Education</h2>
-              <p>
-                Engage and excite students and professionals in interior design
-                fields about horticulture.
-              </p>
-            </div>
-            <div className="vision-box">
-              <img src="Empower.png" />
-              <h2>Empowerment</h2>
-              <p>
-                Empower creative moms by creating and popularizing business
-                opportunities.
-              </p>
-            </div>
-            <div className="vision-box">
-              <img src="Partnership.png" />
-              <h2>Partnerships</h2>
-              <p>
-                Cultivate and strengthen partnerships and collaborations, while
-                diversifying and broadening the base of advocates and supporters.
-              </p>
-            </div>
-            <div className="vision-box">
-              <img src="Eco Friendly.png" />
-              <h2>Eco-Friendly</h2>
-              <p>
-                Popularize eco-friendly concepts in the interior design field by
-                advocating the use of recycled materials.
-              </p>
-            </div>
-            <div className="vision-box">
-              <img src="Resource Dev.png" />
-              <h2>Resource Development</h2>
-              <p>
-                Create educational resources such as plant databases, valuable
-                tips, and guides for the community.
-              </p>
-            </div>
-            <div className="vision-box">
-              <img src="Workforce.png" />
-              <h2>Workforce Developement</h2>
-              <p>
-                Increase the skilled workforce in plantscaping and develop a
-                strong pipeline of future talent.
-              </p>
-            </div>
+        <div className="boxes">
+          <div className="vision-box">
+            <img src={"Awareness.png"} />
+            <h2>Awareness</h2>
+            <p>
+              Create a favorable perception of interior plantscaping and
+              increase awareness of careers working with plants.
+            </p>
           </div>
+          <div className="vision-box">
+            <img src="Education.png" />
+            <h2>Education</h2>
+            <p>
+              Engage and excite students and professionals in interior design
+              fields about horticulture.
+            </p>
+          </div>
+          <div className="vision-box">
+            <img src="Empower.png" />
+            <h2>Empowerment</h2>
+            <p>
+              Empower creative moms by creating and popularizing business
+              opportunities.
+            </p>
+          </div>
+          <div className="vision-box">
+            <img src="Partnership.png" />
+            <h2>Partnerships</h2>
+            <p>
+              Cultivate and strengthen partnerships and collaborations, while
+              diversifying and broadening the base of advocates and supporters.
+            </p>
+          </div>
+          <div className="vision-box">
+            <img src="Eco Friendly.png" />
+            <h2>Eco-Friendly</h2>
+            <p>
+              Popularize eco-friendly concepts in the interior design field by
+              advocating the use of recycled materials.
+            </p>
+          </div>
+          <div className="vision-box">
+            <img src="Resource Dev.png" />
+            <h2>Resource Development</h2>
+            <p>
+              Create educational resources such as plant databases, valuable
+              tips, and guides for the community.
+            </p>
+          </div>
+          <div className="vision-box">
+            <img src="Workforce.png" />
+            <h2>Workforce Developement</h2>
+            <p>
+              Increase the skilled workforce in plantscaping and develop a
+              strong pipeline of future talent.
+            </p>
+          </div>
+        </div>
       </div>
       <MainCont>
-        <div className = "bannerOne">
-          <div className = "oneText">
+        <ProductGrid title={"Categories"} type={"CATEGORY"} data={category1} />
+
+        <div className="bannerOne">
+          <div className="oneText">
             <h2>Our Vision</h2>
-            <p>To build a community that appreciates and values eco-friendly interior designs and to raise awareness of the importance of eco-friendly interior decorations through raising awareness, education, workforce development, partnerships, and resource development.</p>
+            <p>
+              To build a community that appreciates and values eco-friendly
+              interior designs and to raise awareness of the importance of
+              eco-friendly interior decorations through raising awareness,
+              education, workforce development, partnerships, and resource
+              development.
+            </p>
           </div>
-          <form action = "/category?type=AllProducts">
-              <input type = "submit" value = "Shop Now"/>
+          <form action="/category?type=AllProducts">
+            <input type="submit" value="Shop Now" />
           </form>
         </div>
-        <ProductGrid title={"Categories"} type={"CATEGORY"} data={category1} />
-        <div className = "bannerTwo">
-          <form action = "/category?type=AllProducts">
-              <input type = "submit" value = "Shop Now"/>
+        <div className="bannerTwo">
+          <form action="/category?type=AllProducts">
+            <input type="submit" value="Shop Now" />
           </form>
-          <div className = "twoText">
+          <div className="twoText">
             <h2>Our Goal</h2>
-            <p>To promote eco-friendly interior designing and
-          inspire people to pursue careers working with plants while finding
-          meaningful creative work to financially support them and their
-          families.</p>
+            <p>
+              To promote eco-friendly interior designing and inspire people to
+              pursue careers working with plants while finding meaningful
+              creative work to financially support them and their families.
+            </p>
           </div>
         </div>
       </MainCont>
