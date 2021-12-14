987450e38c7968b015ef418d4b556286f60e0001
Msabdulla17 <33076735+Msabdulla17@users.noreply.github.com>
2021-12-13 19:09:33 -0500

    fix?

--- next.config.js
+++ next.config.js
@@ -7,4 +7,10 @@ module.exports = {
 
     return config;
   },
+  env: {
+    'MYSQL_HOST':'138.128.160.186',
+    'MYSQL_DATABASE':'cokedama_products',
+    'MYSQL_USERNAME':'testuser',
+    'MYSQL_PASSWORD':'X;th7h04GN@q',
+  }
 };
