0cb79cd29c912d0bd9ca0d26c5e4a10f12bfccd3
Msabdulla17 <33076735+Msabdulla17@users.noreply.github.com>
2021-12-13 22:33:35 -0500

    a

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ module.exports = {
     return config;
   },
   env: {
-    'MYSQL_HOST':'138.128.160.186',
+    'MYSQL_HOST':'localhost',
     'MYSQL_DATABASE':'cokedama_products',
     'MYSQL_USERNAME':'cokedama_testuser',
     'MYSQL_PASSWORD':'X;th7h04GN@q',
