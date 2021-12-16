4be1460a1f760b1a77849c04fc5c5a90e5aca6e5
Msabdulla17 <33076735+Msabdulla17@users.noreply.github.com>
2021-12-16 17:02:03 -0500

    a

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -1,34 +1,34 @@
-// import { sql_query } from "../../lib/db";
+import { sql_query } from "../../lib/db";
 
-// const handler = async (_, res) => {
-//     try {
-//         const results = await sql_query(`
-//             SELECT * FROM products
-//              ORDER BY id
-//         `);
-//         return res.json(results);
-//     } catch (e) {
-//         res.status(500).json({ message: e.message });
-//     }
-// };
+const handler = async (_, res) => {
+    try {
+        const results = await sql_query(`
+            SELECT * FROM products
+             ORDER BY id
+        `);
+        return res.json(results);
+    } catch (e) {
+        res.status(500).json({ message: e.message });
+    }
+};
 
-// export default handler;
+export default handler;
 
-const {
-	createPool
-} = require('mysql');
+// const {
+// 	createPool
+// } = require('mysql');
 
-const pool = createPool({
-	host: "192.168.64.2",
-	user: "cokedama_user1",
-	password: "nospecialchars123",
-	database: "cokedama_products",
-	connectionLimit: 10
-})
+// const pool = createPool({
+// 	host: "192.168.64.2",
+// 	user: "cokedama_user1",
+// 	password: "nospecialchars123",
+// 	database: "cokedama_products",
+// 	connectionLimit: 10
+// })
 
-pool.query(`select * from products`, (err, result, fields)=>{
-	if (err) {
-		return console.log(err);
-	}
-	return console.log(result);
-})
+// pool.query(`select * from products`, (err, result, fields)=>{
+// 	if (err) {
+// 		return console.log(err);
+// 	}
+// 	return console.log(result);
+// })
