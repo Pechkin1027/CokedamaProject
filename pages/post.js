8a71de9fc97bf384d1e75335d4d22f70742d3695
PillarManJ <51094430+PillarManJ@users.noreply.github.com>
2021-12-16 19:10:22 -0500

    .

---
+++ pages/post.js
@@ -0,0 +1,14 @@
+import { sql_queary } from "../lib/db";
+
+const handler = async (_,res) => {
+    try {
+        `SELECT * FROM cokedama_products
+        ORDER 
+        
+        ` 
+    }catch (e){
+
+    }
+};
+
+export default handler; 
